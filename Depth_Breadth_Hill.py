a8257480d7cab481bf150fb78666056a2ed72ea2
Haiyun <angela.happy123456@gmail.com>
2022-08-01 07:30:33 -0400

    update

--- Depth_Breadth_Hill.py
+++ Depth_Breadth_Hill.py
@@ -1,11 +1,3 @@
-'''
-Find the goal from the Start Node using uninformed strategies (Depth First and
-Breadt First) and see what happens. Then proceed to an informed strategy
-(Hill climbing). 
-a) A Depth First Search
-b) A breadth First Search
-c) Hill climbing
-'''
 
 
 from collections import defaultdict
